e14f09fa99b42c4c85ba1cfdb505c415399cb10a
Devlin Verley <verle006@umn.edu>
2018-03-19 02:50:57 -0500

    luckySevens progress

--- luckySevens/luckySevens.js
+++ luckySevens/luckySevens.js
@@ -1,17 +1,51 @@
 //    Play    //
 function play() {
-        var bet = document.forms["play"].value;
+        var bet = document.forms["sevensBet"]["bet"].value;
         var startingBet = bet;
+        var balance = bet;
         var highestWon = 0;
         var rollCountAtHighest = 0;
+        var diceSum = 0;
+
         if (bet = 0) {
                 validateItems();
         }
+        if (bet > 0) {
+                do {
+                        rollDice();
+                        console.log("Dice Sum: " + diceSum);
+                        if (diceSum == 7) {
+                                balance += 4;
+                                console.log("Balance: " + balance);
+                                if (balance > highestWon) {
+                                        highestWon = balance;
+                                        rollCountAtHighest = totalRolls;
+                                }
+                                totalRolls++;
+                        }
+                        else {
+                                balance -= 1;
+                                console.log("Balance: " + balance);
+                        }
+                } while (balance > 0);
+                if (balance = 0) {
+                        printResults();
+                }
+        }
 }
 
-//    Roll Dice (1-6)    //
+//    Roll Two Dice    //
 function rollDice() {
-        return Math.ceil(Math.random() * (1 + 6 - 1));
+        var dice1 = Math.ceil(Math.random() * (1 + 6 - 1));
+        var dice2 = Math.ceil(Math.random() * (1 + 6 - 1));
+        var diceSum = dice1 + dice2;
+        return diceSum;
+}
+
+//    Print Results    //
+function printResults() {
+        // Insert show table and results here
+        playAgain();
 }
 
 //    Clear Errors    //
@@ -27,7 +61,13 @@ function clearErrors() {
 function validateItems() {
         clearErrors();
         var bet = document.forms["play"].value;
-        if (bet == "" || isNaN(bet) || == 0) {
+        if (bet == "" || isNaN(bet)) {
+                alert("Bet must be filled in with a number.");
+                document.forms["play"]["bet"].parentElement.className = "form-group has-error";
+                document.forms["play"]["bet"].focus();
+                return false;
+        }
+        else if (bet == 0) {
                 alert("Bet must be filled in with a number greater than zero.");
                 document.forms["play"]["bet"].parentElement.className = "form-group has-error";
                 document.forms["play"]["bet"].focus();
